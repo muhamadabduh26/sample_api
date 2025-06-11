30f544334265948542325a9929fd268c2f321b81
muhamadabduh26 <115602540+muhamadabduh26@users.noreply.github.com>
2025-06-11 23:51:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/nasabah", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`API berjalan di port ${PORT}`);
 });
